bd501749c0ccb53313658ed5c44a8c64329a209b
Vinicius Ferrer de Queiroz Eloy <82046286+Jhoinnn@users.noreply.github.com>
2022-08-19 11:26:35 -0300

    Add files via upload

---
+++ aulaaa/src/aulaaa/Aulaaaa.java
@@ -0,0 +1,47 @@
+package aulaaa;
+//import java.io.RandomAccessFile;//
+public class Aulaaaa {
+//Metodo de leitura pedido no enunciado da quest�o//
+	
+	public static int somando(int numA,int numB)//Método com retorno em int para soma//
+	   {
+	  int resposta;
+	  
+	  resposta=numA+numB;
+	  
+	   
+	   
+	   
+	   return resposta;
+	   
+	   }
+		
+		
+		
+	public static void main(String[] args)
+	{
+	 int a,b;
+	 int resp;
+	int Continuar=1;
+	 	
+	
+		
+		
+		while(Continuar==1) {//Repetição para parar quando pedido//
+			MyIO.print("Digite o primeiro numero ");
+		 	a=MyIO.readInt();
+		 	MyIO.print("Digite o segundo numero ");
+			b=MyIO.readInt();
+		resp=somando(a,b);
+		
+		MyIO.println("A soma eh "+resp);
+		
+		MyIO.print("Deseja continuar?(1)Sim (2)Nao ");
+		
+		Continuar=MyIO.readInt();
+		
+		}
+		
+	}  
+	}
+		
